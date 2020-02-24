2429a72843daa8a8e41ea7cd7d0a416da5f00d10
theMINAD <steameetu@gmail.com>
2020-02-24 03:45:06 +0200

    Update echo.py

--- examples/echo.py
+++ examples/echo.py
@@ -2,21 +2,18 @@ import asyncio
 import aconsole
 
 
-async def main():
-    loop = asyncio.get_event_loop()
-    console = aconsole.AsyncConsole()
-
-    loop.create_task(console.mainloop())
-    while True:
-        while True:
-            i = await console.input("echo to out: ")
-            if i:
-                break #Not none or empty string.
-            
-        await console.print("echo: %s"%i)
-        await asyncio.sleep(0.5)
-
-
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    
+    console = AsyncConsole()
+    console.title('echo test')
+    console.set_alpha(0.9)
+
+    async def echo():
+        while True:
+            result = await console.input('echo: ')
+            console.print('you typed:', result)
+
+    with suppress(asyncio.CancelledError):
+        console.run()
+        loop.run_until_complete(echo())
