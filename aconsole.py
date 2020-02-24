f5ca6183cca9433fe685b9d55d12de937521ba01
theMINAD <steameetu@gmail.com>
2020-02-24 03:14:48 +0200

    Update aconsole.py
    bug fixes

--- aconsole.py
+++ aconsole.py
@@ -1,230 +1,247 @@
+import tkinter
 import asyncio
-import os.path
-import tkinter as tk
+import time
+from contextlib import suppress
 
 
-class AsyncConsole(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class AsyncConsole(tkinter.Tk):
+    def __init__(self, **tkargs):
+        # super init
+        super().__init__(**tkargs)
 
-        #basic values:
-        self.__history = []
-        self.__history_pos = 0
+        # input
+        self.__input_queue = None
         self.__input_future = None
-        self.__input_queue = asyncio.Queue()
-        self.__output_queue = asyncio.Queue()
+        self.__input_loop_task = None
+        self.__input_history = []
+        self.__input_history_pos = 0
 
-        #gui color options:
-        self.__foreground_c = 'green'
-        self.__background_c = 'black'
-        self.__disabled_c = 'dimgray'
+        # ui
+        self.__output_frame = tkinter.Frame(self)
+        self.__output_scroll = tkinter.Scrollbar(self.__output_frame)
+        self.__output_text = tkinter.Text(self.__output_frame)
 
-        #gui components:
-        self.__output_frame = tk.Frame(self)
-        self.__output_scroll = tk.Scrollbar(self.__output_frame)
-        self.__output = tk.Text(self.__output_frame)
+        self.__input_frame = tkinter.Frame(self)
+        self.__input_prompt = tkinter.Label(self.__input_frame)
+        self.__input_text = tkinter.Entry(self.__input_frame)
 
-        self.__input_frame = tk.Frame(self)
-        self.__input_prompt = tk.Label(self.__input_frame)
-        self.__input = tk.Entry(self.__input_frame)
+        # state
+        self.__running = False
+        self.__destroyed = False
+        self.__done = None
 
-        #build the style for components
-        self.title(os.getcwd())
-        self._config_ui()
+        self.__init_ui()
 
-    def _config_ui(self):
+    @property
+    def running(self):
+        return self.__running and not self.__destroyed
+
+    def __init_ui(self):
+        self.protocol('WM_DELETE_WINDOW', self.__win_destroyed)
+
+        self.wait_visibility(self)
+        self.set_colors('black', 'green2')
+
         self.config(bd=1, highlightthickness=0)
 
         self.__output_frame.config(bd=1, highlightthickness=0)
 
-        self.__output_scroll.config(orient=tk.VERTICAL, command=self.__output.yview)
+        self.__output_scroll.config(
+            orient=tkinter.VERTICAL, command=self.__output_text.yview)
         self.__output_scroll.config(bd=0)
-        
-        self.__output.config(bd=0, highlightthickness=0)
-        self.__output.config(font=("Courier New", 14))
-        self.__output.config(state=tk.DISABLED)
 
-        self.__output_frame.pack(fill=tk.BOTH, expand=tk.YES)
-        self.__output_scroll.pack(fill=tk.Y, side=tk.RIGHT)
-        self.__output.pack(fill=tk.BOTH, expand=tk.YES)
+        self.__output_text.config(bd=0, highlightthickness=0)
+        self.__output_text.config(font=('Courier New', 14))
+        self.__output_text.bind('<Key>', self.__output_key_press)
+
+        self.__output_frame.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+        self.__output_scroll.pack(fill=tkinter.Y, side=tkinter.RIGHT)
+        self.__output_text.pack(fill=tkinter.BOTH, expand=tkinter.YES)
 
         self.__input_frame.config(bd=2, highlightthickness=0)
 
-        self.__input.config(font=("Courier New", 15))
-        self.__input.config(bd=0, highlightthickness=0)
+        self.__input_text.config(font=('Courier New', 15))
+        self.__input_text.config(bd=0, highlightthickness=0)
         self.__input_prompt.config(bd=0, highlightthickness=0)
-        self.__input_prompt.config(font=("Courier New", 15))
-        self.__input_prompt.config(text=">")
+        self.__input_prompt.config(font=('Courier New', 15))
+        self.__input_prompt.config(text='>')
 
-        self.__input_frame.pack(fill=tk.X)
-        self.__input_prompt.pack(side=tk.LEFT)
-        self.__input.pack(fill=tk.X)
+        self.__input_frame.pack(fill=tkinter.X)
+        self.__input_prompt.pack(side=tkinter.LEFT)
+        self.__input_text.pack(fill=tkinter.X)
 
-        self.__input.bind('<Return>', (lambda _: self._finish_input()))
-        self.__input.bind('<Up>', (lambda _: self._nav_up_input()))
-        self.__input.bind('<Down>', (lambda _: self._nav_down_input()))
+        self.__input_text.bind('<Return>', (lambda _: self.__input_finish()))
+        self.__input_text.bind('<Up>', (lambda _: self.__input_nav_up()))
+        self.__input_text.bind('<Down>', (lambda _: self.__input_nav_down()))
 
-        self._disable_input()
-        self._update_colors()
+        self.__input_disable()
 
-    def _update_colors(self):
-        self.__output_frame.config(background=self.__foreground_c)
+    def __win_destroyed(self):
+        self.__destroyed = True
+        self.destroy()
 
-        self.__output_scroll.config(background=self.__background_c)
+    def __output_key_press(self, key):
+        try:
+            if (key.state == 4 or key.state == 6) and key.keysym.lower() == 'c':
+                text = self.__output_text.get(tkinter.SEL_FIRST, tkinter.SEL_LAST)
+                self.clipboard_clear()  
+                self.clipboard_append(text)  
+            elif (key.state == 4 or key.state == 6) and key.keysym.lower() == 'r':
+                self.__output_text.delete('1.0', tkinter.END)
+        except tkinter.TclError:
+            pass
+        
+        return 'break'
 
-        self.__output.config(background=self.__background_c, foreground=self.__foreground_c)
+    def __input_nav_up(self):
+        if len(self.__input_history) == 0: return
+        
+        self.__input_history_pos -= 1
+        if self.__input_history_pos < 0:
+            self.__input_history_pos = 0
+        if self.__input_history_pos >= len(self.__input_history):
+            self.__input_history_pos = len(self.__input_history) -1
 
-        self.__output.config(insertbackground=self.__foreground_c)
-        self.__output.config(selectforeground=self.__background_c)
-        self.__output.config(selectbackground=self.__foreground_c)
+        self.__input_text.delete(0, tkinter.END)
+        self.__input_text.insert(tkinter.END, self.__input_history[self.__input_history_pos])
 
-        self.__input_frame.config(bg=self.__foreground_c)
+    def __input_nav_down(self):
+        if len(self.__input_history) == 0: return
 
-        self.__input_prompt.config(background=self.__background_c, foreground=self.__foreground_c)
-
-        self.__input.config(background=self.__background_c, foreground=self.__foreground_c)
-        self.__input.config(insertbackground=self.__foreground_c)
-        self.__input.config(selectforeground=self.__background_c)
-        self.__input.config(selectbackground=self.__foreground_c)
-        self.__input.config(disabledbackground=self.__background_c)
-
-    def _nav_down_input(self):
-        if len(self.__history) == 0: return
-
-        self.__history_pos += 1
-        if self.__history_pos >= len(self.__history):
-            self.__history_pos = len(self.__history)
-            self.__input.delete(0, tk.END)
+        self.__input_history_pos += 1
+        if self.__input_history_pos >= len(self.__input_history):
+            self.__input_history_pos = len(self.__input_history)
+            self.__input_text.delete(0, tkinter.END)
             return
 
-        self.__input.delete(0, tk.END)
-        self.__input.insert(tk.END, self.__history[self.__history_pos])
+        self.__input_text.delete(0, tkinter.END)
+        self.__input_text.insert(tkinter.END, self.__input_history[self.__input_history_pos])
 
-    def _nav_up_input(self):
-        if len(self.__history) == 0: return
-        
-        self.__history_pos -= 1
-        if self.__history_pos < 0:
-            self.__history_pos = 0
-        if self.__history_pos >= len(self.__history):
-            self.__history_pos = len(self.__history) -1
+    def __input_finish(self):
+        if self.__input_future and not self.__input_future.done():
+            result = self.__input_text.get()
+            self.__input_future.set_result(result)
+            self.__input_save(result)
 
-        self.__input.delete(0, tk.END)
-        self.__input.insert(tk.END, self.__history[self.__history_pos])
+    def __input_save(self, result):
+        if result:
+            if self.__input_history:
+                last = self.__input_history[len(self.__input_history) - 1]
+                if last != result:
+                    self.__input_history.append(result)
+            else:
+                self.__input_history.append(result)
 
-    def _disable_input(self):
-        self.__input.config(state=tk.DISABLED)
-        self.__input_prompt.config(state=tk.DISABLED)
-        self.__input_prompt.config(text=">")
+        self.__input_history_pos = len(self.__input_history)
 
-    def _enable_input(self, input_str, result):
-        self.__input.config(state=tk.NORMAL)
-        self.__input_prompt.config(state=tk.NORMAL)
-        self.__input_prompt.config(text=input_str)
+    def __input_enable(self, prompt):
+        self.__input_text.config(state=tkinter.NORMAL)
+        self.__input_prompt.config(state=tkinter.NORMAL)
+        self.__input_prompt.config(text=prompt)
 
-        self.__input_future = result
+    def __input_disable(self):
+        self.__input_text.delete(0, tkinter.END)
+        self.__input_text.config(state=tkinter.DISABLED)
+        self.__input_prompt.config(state=tkinter.DISABLED)
+        self.__input_prompt.config(text='>')
 
-    def _save_input(self):
-        i = self.__input.get()
-        l = len(self.__history)
-
-        if len(i) != 0:
-            if l == 0:
-                self.__history.append(i)
-            elif self.__history[l-1] != i:
-                self.__history.append(i)
-
-        self.__history_pos = l+1
-
-    def _finish_input(self):
-        if self.__input_future != None:
-            if not self.__input_future.done():
-                self._save_input()
-
-                self.__input_future.set_result(self.__input.get())
-                self.__input.delete(0, tk.END)
-                self.__input_future = None
-            
-    async def _input_loop(self):
+    async def __input_loop(self):
         while True:
-            input_str, completed = await self.__input_queue.get()
+            self.__input_future, prompt = await self.__input_queue.get()
+
+            if self.__input_future.done():
+                self.__input_queue.task_done()
+                continue
             
-            input_future = asyncio.Future()
-            self._enable_input(input_str, input_future)
+            self.__input_enable(prompt)
 
-            result = await input_future
-            completed.set_result(result)
-            self._disable_input()
+            with suppress(asyncio.CancelledError):
+                await self.__input_future
 
-    async def _output_loop(self):
-        while True:
-            print_str, completed = await self.__output_queue.get()
+            if self.__destroyed or not self.__running:
+                break
 
-            self.__output.config(state=tk.NORMAL)
-            self.__output.insert(tk.END, print_str)
-            self.__output.see(tk.END)
-            self.__output.config(state=tk.DISABLED)
+            self.__input_disable()
+            self.__input_queue.task_done()
 
-            completed.set_result(None)
+    def input(self, prompt):
+        if self.__destroyed or not self.__running:
+            raise RuntimeError('console closed')
 
-    def set_foreground(self, color):
-        self.__foreground_c = color
-        self._update_colors()
+        future = asyncio.Future()
+        self.__input_queue.put_nowait((future, prompt))
+        return future
 
-    def set_background(self, color):
-        self.__background_c = color
-        self._update_colors()
+    def print(self, *args, sep=' ', end='\n'):
+        if self.__destroyed or not self.__running:
+            raise RuntimeError('console closed')
 
-    def set_disabled(self, color):
-        self.__disabled_c = color
-        self._update_colors()
-
-    def clear_output(self):
-        self.__output.config(state=tk.NORMAL)
-        self.__output.delete(1.0, tk.END)
-        self.__output.config(state=tk.DISABLED)
+        print_str = sep.join(str(x) for x in args) + end
+        self.__output_text.insert(tkinter.END, print_str)
+        self.__output_text.see(tkinter.END)
 
     def cancel_input(self):
-        if self.__input_future != None:
-            if not self.__input_future.done():
-                self.__input_future.set_result(None)
-                self.__input.delete(0, tk.END)
-            
-    async def print(self, *args, sep=' ', end='\n'):
-        print_str = sep.join(str(x) for x in args) + end
-        completed = asyncio.Future()
+        if self.__input_future and not self.__input_future.done():
+            self.__input_disable()
+            self.__input_future.set_exception(asyncio.CancelledError('input cancelled'))
 
-        await self.__output_queue.put((print_str, completed))
-        await completed
-
-    def print_no_wait(self, *args, sep=' ', end='\n'):
-        print_str = sep.join(str(x) for x in args) + end
-
-        self.__output.config(state=tk.NORMAL)
-        self.__output.insert(tk.END, print_str)
-        self.__output.see(tk.END)
-        self.__output.config(state=tk.DISABLED)
-
-    async def input(self, prompt):
-        completed = asyncio.Future()
-
-        await self.__input_queue.put((prompt, completed))
-
-        result = await completed
-        return result
+    def set_colors(self, background, foreground):
+        self.__output_frame.config(background=foreground)
+        self.__output_scroll.config(background=background)
+        self.__output_text.config(background=background, foreground=foreground)
+        self.__output_text.config(insertbackground=foreground)
+        self.__output_text.config(selectforeground=background)
+        self.__output_text.config(selectbackground=foreground)
+        self.__input_frame.config(bg=foreground)
+        self.__input_prompt.config(background=background, foreground=foreground)
+        self.__input_text.config(background=background, foreground=foreground)
+        self.__input_text.config(insertbackground=foreground)
+        self.__input_text.config(selectforeground=background)
+        self.__input_text.config(selectbackground=foreground)
+        self.__input_text.config(disabledbackground=background)
+    
+    def set_alpha(self, opacity):
+        self.attributes('-alpha', opacity)
+        self.wm_attributes('-alpha', opacity)
 
     async def mainloop(self):
-        loop = asyncio.get_event_loop()
+        if self.__running:
+            raise RuntimeError('aready running')
 
-        loop.create_task(self._output_loop())
-        loop.create_task(self._input_loop())
+        if self.__destroyed:
+            raise RuntimeError('destroyed')
 
-        while True:
-            self.update()
-            await asyncio.sleep(1/60) #60 fps
+        try:
+            loop = asyncio.get_event_loop()
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    cmd = AsyncCmd()
+            self.__input_queue = asyncio.Queue()
+            self.__input_loop_task = loop.create_task(self.__input_loop())
+            self.__running = True
 
-    loop.run_until_complete(cmd.mainloop())
+            while True:
+                self.update()
+                self.update_idletasks()
+                await asyncio.sleep(0.01)
+        except tkinter.TclError:
+            pass
+        finally:
+            self.__running = False
+
+            if not self.__input_loop_task.done():
+                self.__input_loop_task.cancel()
+
+            if self.__input_future and not self.__input_future.done():
+                self.__input_future.set_exception(asyncio.CancelledError('console closed'))
+
+            while not self.__input_queue.empty():
+                future, _ = self.__input_queue.get_nowait()
+                future.set_exception(asyncio.CancelledError('console closed'))
+                
+            with suppress(asyncio.CancelledError):
+                await self.__input_loop_task
+
+    def run(self, loop=None):
+        if not loop:
+            loop = asyncio.get_event_loop()
+
+        return loop.create_task(self.mainloop())
